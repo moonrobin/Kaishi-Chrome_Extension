2242430eeaeb0014a0882128bbc5ba4afed8f81e
moonrobin <moonrobin@hotmail.com>
2015-05-10 20:03:36 -0400

    Added icons

--- config.js
+++ config.js
@@ -1,9 +1,19 @@
 function save() {
-	var albumID = document.getElementById("album").value;
-	chrome.storage.sync.set({
-		imgurAlbumID: albumID
-	});
-	console.log('Imgur album with ID ' + albumID + ' has been saved.');
+	var albumID = document.getElementById("imguralbum").value;
+//	chrome.storage.sync.set({
+//		imgurAlbumID: albumID
+//	}, function() {
+//		console.log('Imgur album with ID ' + albumID + ' has been saved.');
+//		message("Settings saved");
+//	});
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", "https://api.imgur.com/3/album/" + albumID, true);
+	xhr.setRequestHeader("Authorization", "Client-ID c16c61562ba10db");
+	xhr.send();
+
+	var response = xhr.responseText;
+
 }
+
 
 document.getElementById("saveButton").addEventListener("click", save);
